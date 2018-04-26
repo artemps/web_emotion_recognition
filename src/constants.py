ee58c6646a38f9e68613140386f6ac362bb40694
Artem Pshenichny <fised@mail.ru>
2018-04-26 15:53:38 +0300

    Added constants and cascade classifier

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+""" File of constants """
+__author__ = 'Artem Pshenichny'
+
+
+EMOTIONS = ['angry', 'disgusted', 'fearful', 'happy', 'sad', 'surprised', 'neutral']
+IMG_SIZE = 48
+CASC_CLASS_MODEL = '../haarcascade_frontalface_default.xml'
+MODEL_DIR = '../model/'
+USER_DATA_DIR = '../user_data/'
